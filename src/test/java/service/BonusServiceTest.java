737c77077ca076d3e43d0e592735edf1faab27d1
thamyris <thamyris.thurler@nuvemshop.com.br>
2022-07-08 15:50:50 -0300

    Iniciado pequenos conceitos de asserts com junit

---
+++ src/test/java/service/BonusServiceTest.java
@@ -0,0 +1,35 @@
+package service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(BigDecimal.ZERO, bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.0"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.0"), bonus);
+    }
+}
